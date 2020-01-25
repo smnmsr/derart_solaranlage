e7f1dcfd71d3a6d850f34806585d3f3f8b7e2259
Simon Moser <simonmoser@MacBook-Pro.localdomain>
2020-01-26 00:14:47 +0100

    alle Includes hinzugefügt

--- main.ino
+++ main.ino
@@ -1,9 +1,9 @@
-#include <Wire.h>
-
-#include <PID_v1.h>
-
-#include <LiquidCrystal.h>
-
+#include <Wire.h> //Für I2C Bus
+#include <PID_v1.h> //Für PID-Regelung
+#include <Adafruit_LiquidCrystal.h> //Für Displays
+#include <pins_adresses.ino> //File, in dem Pins und Adressen definiert sind
+#include <variables.ino>
+#include <functions.ino>
 
 void setup() {
 
