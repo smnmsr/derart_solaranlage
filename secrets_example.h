b80ac3f46f444288968312c5900ec2e916cde3e4
Simon Moser <simonmoser@MoserArtzMac.local>
2020-04-16 10:10:11 +0200

    added the secrets example

---
+++ secrets_example.h
@@ -0,0 +1,3 @@
+#define MQTT_USER "<mymqttusername>" //the Username to your MQTT Broker
+#define MQTT_PASS "<mymqttpassword>" //the Password to your MQTT Broker
+#define SMS_BASE_URL "GET /API/httpsms.php?konto=<userid>&password=<apikey>&service=<serviceid>&text="
